524738079b81a951f8e6b141a50c542436af59f1
Maxiim3 <maximemickael.contact@gmail.com>
2025-05-11 20:08:41 +0200

    WIP - add feature comment

--- app/views/app.tsx
+++ app/views/app.tsx
@@ -20,8 +20,13 @@ export default function App() {
 		<main className={cn(bg.bgImage, "w-screen h-screen")}>
 			<TopBar />
 			<WindowProvider>
+				{/* add <Desktop/> */}
 				<RenderWindowApps />
 			</WindowProvider>
 		</main>
 	)
 }
+
+// Prepared everything. Now implement how to open windows from processes.
+// use tanstack store ?
+// create Desktop component to add a few icons off apps. they will trigger new processes with singleton for each (uniq instance)
