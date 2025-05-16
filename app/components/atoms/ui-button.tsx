c4505badea5fd22b9bfee09dee305d13e5e07ee3
Maxiim3 <maximemickael.contact@gmail.com>
2025-05-16 12:58:40 +0200

    feature - remove event on button elements so that it hides its children

--- app/components/atoms/ui-button.tsx
+++ app/components/atoms/ui-button.tsx
@@ -4,7 +4,7 @@ const UIButton: FC<ComponentPropsWithoutRef<"button">> = props => {
 	return (
 		<button
 			{...props}
-			className="hover:cursor-pointer focus:bg-gray-100/40 active:bg-gray-100/40 py-[2px] px-3 rounded-sm">
+			className="cursor-none focus:bg-gray-100/40 active:bg-gray-100/40 py-[2px] px-3 rounded-sm *:select-none *:pointer-events-none">
 			{props.children}
 		</button>
 	)
